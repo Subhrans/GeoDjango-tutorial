6531850c1ec79b7185c5663e52ee867bf8949c02
subhransu das <subhransud525@gmail.com>
2021-01-30 12:30:43 +0530

    map works by python geopy2

--- GeoDjango/utils.py
+++ GeoDjango/utils.py
@@ -2,12 +2,16 @@ from django.contrib.gis.geoip2 import GeoIP2
 
 
 def get_ip(ip):
-    user_ip=GeoIP2()
-    country=user_ip.country(ip)
-    city=user_ip.city(ip)
-    lat,long=user_ip.lat_lon(ip)
+    internet_error=None
+    try:
+        user_ip=GeoIP2()
+        country=user_ip.country(ip)
+        city=user_ip.city(ip)
+        lat,long=user_ip.lat_lon(ip)
+    except ConnectionError:
+        internet_error=True
 
-    return country,city,lat,long
+    return country,city,lat,long,internet_error
 
 
 
--- GeoDjango/views.py
+++ GeoDjango/views.py
@@ -13,37 +13,48 @@ def calculate_distance_view(request):
     geolocator=Nominatim(user_agent='GeoDjango')        # initialize app folder with nominatim
 
     my_ip='72.14.207.99'
-    city,country,lat,long=get_ip(my_ip)
-    country=geolocator.geocode(country)
-    print("country",country)
-    print("city",city)
-    print("lat",lat)
-    print("long",long)
-    location_lat_lon=(lat,long)
+    city,country,lat,long,internet_error=get_ip(my_ip)
+    if internet_error==None:
+        country=geolocator.geocode(country)
+        print("country",country)
+        print("city",city)
+        print("lat",lat)
+        print("long",long)
+        location_lat_lon=(lat,long)
 
-    # Initial Folium Map
-    map=folium.Map(location=location_lat_lon,width=800,height=500)
-    if mform.is_valid():
-        instance=mform.save(commit=False)
-        # instance.destination=mform.cleaned_data['destination']
-        destination_=mform.cleaned_data['destination']
-        destination=geolocator.geocode(destination_)        # geocode function takes one or more location name and return all information about the location
-        lat=destination.latitude
-        long=destination.longitude
-        print("lat: ",lat)
-        print("long: ",long)
-        destination_lat_lon=(lat,long)
+        # Initial Folium Map
+        map = folium.Map(location=location_lat_lon, width=800, height=500)
+        folium.CircleMarker(location=[lat,long],radius=50,tooltip=city['country_name'],fill=True,fill_color="#428bca").add_to(map)
+        if mform.is_valid():
+            instance=mform.save(commit=False)
+            # instance.destination=mform.cleaned_data['destination']
+            destination_=mform.cleaned_data['destination']
+            destination=geolocator.geocode(destination_)        # geocode function takes one or more location name and return all information about the location
+            lat=destination.latitude
+            long=destination.longitude
+            print("lat: ",lat)
+            print("long: ",long)
+            destination_lat_lon=(lat,long)
+            map = folium.Map(location=destination_lat_lon, width=800, height=500)
+            folium.Marker(location=[lat,long],tooltip=destination_,icon=folium.Icon(color='red')).add_to(map)
+            folium.Circle(location=[lat,long],radius=10,tooltip=destination_).add_to(map)
+            # folium.FeatureGroup(destination_lat_lon)
+            map.fit_bounds(destination_lat_lon,location_lat_lon)
+            # map=folium.Map()
 
-        print(destination)
-        instance.location="Asansol"
-        instance.distance=50.00
-        # Folium Map modification
-        # instance.save()
-        print(instance.destination)
+            print(destination)
+            instance.location="Asansol"
+            instance.distance=50.00
+            # Folium Map modification
+            # instance.save()
+            print(instance.destination)
 
-    map=map._repr_html_()
+        map=map._repr_html_()
+    else:
+        internet_error="Please connect Your Internet"
     context = {'distance': obj,
                'form':mform,
                'map':map,
+               'internet_error':internet_error,
                }
     return render(request, 'Measurements/measurements.html', context)
